94f3bc1ebf0e8f95eff8f73b741eace69c08cf5b
no2don <no2don@DESKTOP-QQ6SH2K>
2017-01-25 16:01:10 +0800

    加入專案檔案。

---
+++ ScriptEngineUtility/Agent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ScriptEngineUtility
+{
+   
+    public class Agent
+    {
+      
+        private static ScriptState<object> scriptState = null;
+
+        public static dynamic Excute(string code)
+        {
+            scriptState = scriptState == null ? CSharpScript.RunAsync(code).Result : scriptState.ContinueWithAsync(code).Result;
+            if (scriptState.ReturnValue != null && !string.IsNullOrEmpty(scriptState.ReturnValue.ToString()))
+                return scriptState.ReturnValue;
+            return null;
+        }
+    }
+}
---
+++ ScriptEngineUtility/TestEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ScriptEngineUtility
+{
+    public class TestEngine
+    {
+        public static void Main()
+        {
+            //Test1 直接 return 
+            Console.WriteLine("==== Test1 => retuen 100 ====");
+            var test1 = Agent.Excute(@"return 100;");
+            Console.WriteLine(test1);
+            Console.WriteLine("");
+
+
+            // Test2 直接 建立Class
+            Console.WriteLine("==== Test2 => retuen dynamic  ====");
+
+            var text = @"
+                    using System;
+                   
+                    public class User
+                    {
+                       public  string Name {get;set;}
+                       public  DateTime Birth {get;set;}
+                    }
+
+                    var user=new User();
+                    user.Name=""許當麻"";
+                    user.Birth=new DateTime(1980,6,21);
+                    return user;
+                    
+                ";
+
+            dynamic test2 = Agent.Excute(
+              text);
+
+            Console.WriteLine(test2.Name + "," + (test2.Birth is DateTime ? (DateTime) test2.Birth : new DateTime()).ToString());
+            Console.WriteLine("");
+
+        }
+    }
+}
